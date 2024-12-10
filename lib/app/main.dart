a0118778d5c4994f79400623f3d6078fcfac5e3a
Nguyen Quang Tho <27039426+Tho391@users.noreply.github.com>
2024-12-10 15:40:17 +0700

    init logger and database in main

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,7 +1,23 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import '../core/database/database_helper.dart';
+import '../core/logging/logging.dart';
 import 'app.dart';
 
 FutureOr<void> main() async {
+  // Initialize the logger
+  Logging.info('Initializing the app');
+
+  // Initialize the database
+  try {
+    await DatabaseHelper.instance.database;
+    Logging.info('Database initialized successfully');
+  } catch (e, stack) {
+    Logging.handleError(e, stack);
+  }
+
+  // Run the app
   runApp(const App());
 }
