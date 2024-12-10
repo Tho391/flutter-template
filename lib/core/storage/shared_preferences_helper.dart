9e146cf228dee8777b61aaa3220fe13fe6d2d57d
Nguyen Quang Tho <27039426+Tho391@users.noreply.github.com>
2024-12-10 15:45:50 +0700

    Create shared_preferences_helper.dart

---
+++ lib/core/storage/shared_preferences_helper.dart
@@ -0,0 +1,36 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesHelper {
+  // This method retrieves a value from shared preferences
+  static Future<String?> getString(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  // This method stores a value in shared preferences
+  static Future<void> setString(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
+  }
+
+  // Other methods for other types like int, bool, double, etc.
+  static Future<bool> setBool(String key, bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    return await prefs.setBool(key, value);
+  }
+
+  static Future<int?> getInt(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(key);
+  }
+
+  static Future<void> setInt(String key, int value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(key, value);
+  }
+
+  static Future<void> remove(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
+  }
+}
